27bf4790efaaa3bbdb2cd0dfad2c2c375b915d3e
FrancoBujakiewicz <franconatahelbuja@gmail.com>
2025-12-08 15:53:48 -0300

    chore: Updating standart

--- build/Discard.js
+++ build/Discard.js
@@ -1,25 +1,16 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import * as DOM from "./DOM.js";
 import * as Transitions from "./Transitions.js";
 import { Pool } from "./Pool.js";
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-window.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void 0, function* () {
+window.addEventListener("DOMContentLoaded", async () => {
     DOM.init();
-    const pool = yield Pool.new();
+    const pool = await Pool.new();
     Transitions.toggle(DOM.startSection);
     DOM.startBtn.addEventListener("click", () => {
         Transitions.toggle(DOM.startSection);
         Transitions.toggle(DOM.categorySection);
         DOM.categories.forEach((category) => {
-            category.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
+            category.addEventListener("click", async () => {
                 const actualCategory = category.id;
                 const actualPool = pool[actualCategory];
                 let count = 0;
@@ -31,12 +22,12 @@ window.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void
                 DOM.h2_1.textContent = actualPool[count].split(".")[0];
                 DOM.img_2.src = `img/${actualCategory}/${actualPool[count + 1]}`;
                 DOM.h2_2.textContent = actualPool[count + 1].split(".")[0];
-                yield delay(10);
-                yield delay(1000);
+                await delay(10);
+                await delay(1000);
                 Transitions.toggle(DOM.optionsArticle);
                 Transitions.toggle(DOM.loading);
                 count = count + 2;
-                DOM.options.forEach((option) => option.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
+                DOM.options.forEach((option) => option.addEventListener("click", async () => {
                     if (count < actualPool.length) {
                         Transitions.toggle(DOM.optionsArticle);
                         Transitions.toggle(DOM.loading);
@@ -44,8 +35,8 @@ window.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void
                         DOM.h2_1.textContent = actualPool[count].split(".")[0];
                         DOM.img_2.src = `img/${actualCategory}/${actualPool[count + 1]}`;
                         DOM.h2_2.textContent = actualPool[count + 1].split(".")[0];
-                        yield delay(10);
-                        yield delay(1000);
+                        await delay(10);
+                        await delay(1000);
                         Transitions.toggle(DOM.loading);
                         Transitions.toggle(DOM.optionsArticle);
                         count = count + 2;
@@ -56,8 +47,8 @@ window.addEventListener("DOMContentLoaded", () => __awaiter(void 0, void 0, void
                         Transitions.toggle(DOM.footer);
                         Transitions.toggle(DOM.endMessage);
                     }
-                })));
-            }));
+                }));
+            });
         });
     });
-}));
+});
--- build/Pool.js
+++ build/Pool.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 export class Pool {
     constructor() {
         this.lang = [];
@@ -16,23 +7,19 @@ export class Pool {
         this.llm = [];
         this.person = [];
     }
-    static new() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const pool = new Pool();
-            yield pool.init();
-            return pool;
-        });
+    static async new() {
+        const pool = new Pool();
+        await pool.init();
+        return pool;
     }
-    init() {
-        return __awaiter(this, void 0, void 0, function* () {
-            const pool = yield fetch("pool.json").then(r => r.json());
-            this.lang = this.shuffle(pool.lang);
-            this.linux = this.shuffle(pool.linux);
-            this.dev = this.shuffle(pool.dev);
-            this.framework = this.shuffle(pool.framework);
-            this.llm = this.shuffle(pool.llm);
-            this.person = this.shuffle(pool.person);
-        });
+    async init() {
+        const pool = await fetch("pool.json").then(r => r.json());
+        this.lang = this.shuffle(pool.lang);
+        this.linux = this.shuffle(pool.linux);
+        this.dev = this.shuffle(pool.dev);
+        this.framework = this.shuffle(pool.framework);
+        this.llm = this.shuffle(pool.llm);
+        this.person = this.shuffle(pool.person);
     }
     shuffle(array) {
         const shuffled = [...array];
